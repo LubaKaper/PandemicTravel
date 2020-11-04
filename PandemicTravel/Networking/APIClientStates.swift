30dad501a5121ab7dd7f6cbee1d2f5f3be9c4c3d
LubaKaper <liubovkaper@pursuit.org>
2020-11-04 16:48:15 -0500

    APIclient for states

---
+++ PandemicTravel/Networking/APIClientStates.swift
@@ -0,0 +1,39 @@
+//
+//  APIClientStates.swift
+//  PandemicTravel
+//
+//  Created by Liubov Kaper  on 11/4/20.
+//  Copyright © 2020 Luba Kaper. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+struct DateDataWrapper: Decodable {
+    let hits: [Date]
+}
+
+struct Date: Decodable, Hashable {
+    let date: String
+    let state: String
+    let cases: Int
+    let deaths: Int
+}
+
+class APIClientStates {
+    public func searchStates(for query: String) -> AnyPublisher<[Date], Error> {
+        let lastdays = 30
+        let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? "new york"
+        
+        let endpoint = "https://disease.sh/v3/covid-19/nyt/states/\(query)?lastdays=\(lastdays)"
+        let url = URL(string: endpoint)!
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: DateDataWrapper.self, decoder: JSONDecoder())
+            .map { $0.hits }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+
+    }
+}
